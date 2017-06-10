eaa4a3042fe67035e5b9aa132a31fee7ac148e57
aditya-patnaik <aditya.narayan.patnaik@gmail.com>
2017-06-10 17:06:10 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 			
 socket.on('connect', function(){
 	console.log('connected to server');
-
-	socket.emit('createMessage', {
-		from: 'Aditya',
-		text: 'Sample text'
-	});
 });
 
 socket.on('disconnect', function(){
