a46fb88f6e5760b0a7bea99d762e616a449e2da5
aditya-patnaik <aditya.narayan.patnaik@gmail.com>
2017-06-11 20:10:55 +0530

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,16 @@ socket.on('newMessage', function(message){
 	$('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message){
+	var li = $('<li></li>');
+	var a = $('<a target="_blank">My current Location</a>')
+	
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+	$('#messages').append(li);
+});
+
 $('#message-form').on('submit', function(evt){
 	evt.preventDefault();
 	
@@ -25,4 +35,20 @@ $('#message-form').on('submit', function(evt){
 	}, function(){
 		$('[name=message]').val('')
 	});
+});
+
+$('#sendLocation').click(function(){
+	if(!navigator.geolocation){
+		return alert('Geolocation not supported');
+	}
+
+	navigator.geolocation.getCurrentPosition(function(position){
+		console.log(position);
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+	}, function(){
+		alert('Unable to fetch your location');
+	})
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate the correct message object', () => {
@@ -12,3 +12,16 @@ describe('generateMessage', () => {
 		expect(message).toInclude({ from, text });
 	});
 });
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var from = 'Tyler';
+		var lat = 15;
+		var lon = 19;
+		var url = `https://www.google.com/maps/?q=${lat},${lon}`;
+		var message = generateLocationMessage(from, lat, lon);
+
+		expect(message.createdAt).toBeA('number');
+		expect(message).toInclude({from, url});
+	});
+})
